103a8d9efe04a0ee27dcc442a519cb2b11bbc28d
Poorna Sai Krishna Kemburu <kemburupoornasai_krishna@epam.com>
2025-05-12 16:18:49 +0530

    Initial commit

---
+++ src/app.ts
@@ -0,0 +1,16 @@
+import express from "express";
+import cors from "cors";
+import helmet from "helmet";
+import userRoutes from "./routes/user.routes";
+import workoutRoutes from "./routes/workout.routes";
+
+const app = express();
+
+app.use(express.json());
+app.use(cors());
+app.use(helmet());
+
+app.use("/api/users", userRoutes);
+app.use('/dev/workouts', workoutRoutes);
+
+export default app;
